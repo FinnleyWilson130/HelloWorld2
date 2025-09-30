d226139d355819886c33eed592df150a894c56cb
FinnleyWilson130 <finnley2.wilson@live.uwe.ac.uk>
2025-09-30 15:40:58 +0100

    calculator

--- HelloWorld2/HelloWorld2.cpp
+++ HelloWorld2/HelloWorld2.cpp
@@ -2,23 +2,49 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
+    std::string operation;
+
     std::cout << "Hello World!\n";
 
     int a = 0;
     int b = 0;
+    
+
+    std::cout << "enter operation (add, sub, mul or div): ";
+    std::cin >> operation;
 
     std::cout << "please enter a the first number : ";
 
     std::cin >> a;
 
-    std::cout << "please enter a the second number to add to the first : ";
+    std::cout << "please enter a the second number : ";
 
     std::cin >> b;
 
-    std::cout << "your number is" << a+b <<std::endl;
+    if (operation == "add") {
+        std:: cout << "result:" << a + b << std::endl;
+    }
+
+    else if (operation == "sub") {
+        std::cout << "result:" << a - b << std::endl;
+    }
+
+    else if (operation == "div") {
+        if (b != 0)
+            std::cout << "result:" << a / b << std::endl;
+        else
+            std::cout << "Error: division by 0" << std::endl;
+    }
+     else if (operation == "mul") {
+        std::cout << "result:" << a * b << std::endl;
+    }
+     else {
+        std::cout << "Unkown Operation!" << std::endl;
+    }
 
     return 0;
 }
